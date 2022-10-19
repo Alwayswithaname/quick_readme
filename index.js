acfbbbf3fb813c5b35a81a33e11de5062a9772af
whl <wplovitz@gmail.com>
2022-10-19 16:03:29 -0700

    functioning readme builder,commit for push

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// TODO: Include packages needed for this application
+
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
-// TODO: Create an array of questions for user input
+
 const questions = [
         {
             type: "input",
@@ -59,17 +59,15 @@ const questions = [
     
     ];
 
-// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
-// TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then((inquirerResponses) => {
         console.log('Generating REAMDME...');
         writeToFile('README.md', generateMarkdown({ ...inquirerResponses}));
     });
 }
-// Function call to initialize app
+
 init();
